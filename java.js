27a9dad701ac7799bb88b7fb6a2e99f473fdd56e
Roycey <Roycedapitan.006@gmail.com>
2024-03-13 03:30:13 +0800

    Added button where it creates a new grid

--- java.js
+++ java.js
@@ -1,31 +1,48 @@
 //Size of the grid
-const gridSize = 16;
-// const resetButton = document.querySelector('button')
+// const gridSize = 16;
+const reset = document.querySelector('button')
 const container = document.querySelector('.container');
-// container.classList.add('container');
 
-const createGrid = (gridAmount) => {
-    //Creates the row of the grid equal to the GridAmount
-    for (let i=0; i < gridAmount; i++){
-        const row = document.createElement('div');
-        row.classList.add('gridrow');
-        //Creates the boxes for the rows
-        for (let j=0; j < gridAmount; j++){
-            const widthHeight = 450/gridSize;
-            const box = document.createElement('div');
-            box.classList.add('gridbox');
-            box.style.width = `${widthHeight}px`;
-            box.style.height = `${widthHeight}px`;
-
-            //change bgcolor with mouseenter
-            box.addEventListener('mouseenter', () => {
-                box.style.backgroundColor = 'black';
-            })
-            row.appendChild(box);
+    const createGrid = (gridAmount) => {
+        const wrapper = document.createElement('div');
+        wrapper.classList.add('wrapper');
+        //Creates the row of the grid equal to the GridAmount
+        for (let i=0; i < gridAmount; i++){
+            const row = document.createElement('div');
+            row.classList.add('gridrow');
+            //Creates the boxes for the rows
+            for (let j=0; j < gridAmount; j++){
+                const widthHeight = 960/gridAmount;
+                const box = document.createElement('div');
+                box.classList.add('gridbox');
+                box.style.width = `${widthHeight}px`;
+                box.style.height = `${widthHeight}px`;
+                //change bgcolor with mouseenter
+                box.addEventListener('mouseenter', () => {
+                    box.style.backgroundColor = 'black';
+                })
+                row.appendChild(box);
+            }
+        wrapper.appendChild(row);
         }
-    container.appendChild(row);
+        container.appendChild(wrapper);
     }
-}
 
-createGrid(gridSize);
-// const div = document.createElement('div');
+
+
+reset.addEventListener('click', () => {
+    let userSize = Number(prompt('How many dimensions do you want for your new grid?'))
+
+    while (userSize > 100){
+        userSize = Number(prompt('Number picked is to big, pick another number that is less than 100 .'))
+    }
+
+    const wrapper = document.querySelector('.wrapper');
+    if (!wrapper){
+        createGrid(userSize)
+    }
+    else{
+        wrapper.remove();
+        createGrid(userSize);
+    }
+})
